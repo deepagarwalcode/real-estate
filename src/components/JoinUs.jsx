d115e730a53dc819c926afeb0445cd654428ba46
DeepAgarwalCodes <deep72003@gmail.com>
2023-11-14 03:27:48 +0530

    appostophee bug 2

--- src/components/JoinUs.jsx
+++ src/components/JoinUs.jsx
@@ -7,7 +7,7 @@ const JoinUs = () => {
     <div className={styles.join_us_container}>
 
         <p>Invest with the industry&apos;s top real estate broker.</p>
-        <p>Elevate your investments by choosing to partner with the industry's leading real estate broker. Experience unparalleled success today.</p>
+        <p>Elevate your investments by choosing to partner with the industry&apos;s leading real estate broker. Experience unparalleled success today.</p>
         <p>Get in touch</p>
     </div>
     </div>
